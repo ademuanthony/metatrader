22a9bb6e9c18642396e9cb67214faf2eba29265b
Ademu Anthony <ademuanthony@gmail.com>
2022-03-27 13:34:30 +0400

    next build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "da5b74c9b5141dbb11d0";
+/******/ 	var hotCurrentHash = "b2341f622f1477d74775";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
